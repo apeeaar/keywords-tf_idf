ab5e1d9e95630480ce0b95496625279760a49bef
apeeaar <46392547+apeeaar@users.noreply.github.com>
2019-03-06 19:20:58 +0530

    Add files via upload

---
+++ Final TFIDF.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import re
+from datetime import date
+from bs4 import BeautifulSoup
+import pymysql
+from urllib.request import Request, urlopen
+from nltk.corpus import stopwords
+import requests
+stopWords = set(stopwords.words('english'))
+
+date1 = str(input("Enter your date in yyyy/mm/dd:"))
+reason1= str(input("Enter your query:"))
+
+year1,month1,day1 =map(int, date1.split('/'))
+
+a= date(2001,1,1)
+b=date(year1,month1,day1)
+var=b-a
+result=var.days + 36892
+link="https://timesofindia.indiatimes.com/"+str(date1)+"/archivelist/year-"+str(year1)+",month-"+str(month1)+",starttime-"+str(result)+".cms"
+
+req= requests.get(link,headers={'User-Agent': 'Mozilla/5.0'})
+
+bsObj= BeautifulSoup(req.text,'lxml')
+
+reason1=reason1.lower()
+tags=bsObj.find_all('a')
+lst=[]
+for tag in tags:
+    var1=tag.get('href')
+    var2=tag.text
+    var2=var2.lower()
+    x=re.findall(reason1,var2)
+    if(len(x)!=0):
+        if("http://timesofindia.indiatimes.com/" or "https://timesofindia.indiatimes.com/" not in var1):
+            var1="http://timesofindia.indiatimes.com/"+str(var1)
+            lst.append(var1)
+        else:
+            lst.append(var1)
+article=[]
+if(len(lst)==0):
+    print("No news found")
+else:
+    for i in lst:
+        html1= requests.get(i,headers={'User-Agent': 'Mozilla/5.0'})
+        bsobj1= BeautifulSoup(html1.text,'lxml')
+        var4= bsobj1.findAll("div",{"class":"Normal"})
+        str1=""
+        try:
+            for j in var4:
+                str1=str1+str(j.text)
+                
+        except:
+            pass
+        article.append(str1)
+article = list(filter(None, article))
+
+for i in range(len(article)):
+    article[i] = article[i].split()
+
+d={}
+for i in article:
+    for x in i:
+        if(x not in d.keys() and x not in stopWords):
+            d[x]=0
+
+for i in range(len(article)): 
+    for word in article[i]:
+        if(word not in stopWords):
+            d[word] +=1
+
+
+count=len(d.keys())
+for i in d.keys():
+    d[i] =d[i]/count
+
+
+pol=[]
+var
+for i in d.keys():
+    pol.append(i)
+
+
+new=[]
+new1=[]
+for i in range(len(pol)):
+    count=0
+    for j in range(len(article)):
+        if(pol[i] in article[j]):
+                count+=1
+    new.append(count)
+    new1.append(count)
+res = {} 
+for key in pol: 
+    for value in new: 
+        res[key] = value 
+        new.remove(value) 
+        break
+
+idf=[]
+import math
+
+for i in range(len(pol)):
+    u=math.log(len(article)/new1[i])
+    idf.append(u)
+
+idfs={}
+for key in pol: 
+    for value in idf: 
+        idfs[key] = value 
+        idf.remove(value) 
+        break
+
+tf_idf=[]
+from collections import OrderedDict 
+for i in range(len(pol)):
+    local=(d[str(pol[i])]) * (idfs[str(pol[i])])
+    tf_idf.append(local)
+final_dic=dict(zip(pol,tf_idf))
+d_sorted_by_value = OrderedDict(sorted(final_dic.items(), key=lambda x: x[1]))
+keywords=list(d_sorted_by_value)
+print(keywords[0:5])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
